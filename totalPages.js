e17758778840d0b8bde2f7c4b390423642d466b8
PanSedtabodee <sedtabodee.boon@kmutt.ac.th>
2022-09-06 10:42:41 +0700

    Test1

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,9 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  console.log(`Test`)
+  let  test1 = 20
+  const testing = test1 > 10 ? 'True':'False'
+  console.log(testing)
 }
 module.exports = totalPages
